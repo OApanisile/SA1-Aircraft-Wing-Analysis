88f0554c2ec63cc721f99afc8cbe285cd19ccc00
Olumide Apanisle <oa320@cam.ac.uk>
2025-05-20 13:55:25 +0100

    Most recent version of Exercise 5

--- Week_1/cylinder_pan.m
+++ Week_1/cylinder_pan.m
@@ -10,6 +10,7 @@ xmin = -2.5;
 xmax = 2.5;
 ymin = -2;
 ymax = 2;
+alpha = 0.1;
 
 % Step (i): Define the cylinder panels
 np = 100;
@@ -19,6 +20,34 @@ ys = sin(theta);  % y-coordinates of panel endpoints
 
 % Step (ii): Define vortex sheet strength at panel edges
 g = -2 * sin(theta);  % vortex sheet strength
+
+function [infa, infb] = refpaninf(del, X, Yin)
+    
+    if abs(Yin) < 1e-5
+        Y = 1e-5;
+    else
+        Y = Yin;
+    end
+
+    % Calculating I0
+    I0 = -(1 / (4 * pi)) * ( ...
+        X*log(X^2 + Y^2) - ...
+        (X-del)*log((X-del)^2 + Y^2) - ...
+        2*del + ...
+        2*Y*(atan(X/Y) - atan((X - del) / Y)) );
+
+    % Calculating I1
+    I1 = 1/(8 * pi) * ((X^2 + Y^2) * log(X^2 + Y^2) ...
+    - ((X - del)^2 + Y^2) * log((X - del)^2 + Y^2) ...
+    - 2 * X * del + del^2);
+
+    % Influence coefficient fa
+    infa = (I0*(1-(X/del)) - (I1/del));
+
+    % Influence coefficient fb
+    infb = (I0*(X/del) + (I1/del));
+  
+end
 
 function [infa, infb] = panelinf(xa, ya, xb, yb, x, y)
     % Vector from A to B
@@ -37,100 +66,113 @@ function [infa, infb] = panelinf(xa, ya, xb, yb, x, y)
     
     % Transform field point to panel coordinates
     X = rx * t(1) + ry * t(2);
-    Yin = rx * n(1) + ry * n(2);
+    Y = rx * n(1) + ry * n(2);
 
-    if abs(Yin) < 1e-5
-        Y = 1e-5;
-    else
-        Y = Yin;
-    end
-
-    % Calculating I0
-    I0 = -(1 / (4 * pi)) * ( ...
-        X*log(X^2 + Y^2) - ...
-        (X-L)*log((X-L)^2 + Y^2) - ...
-        2*L + ...
-        2*Y*(atan(X/Y) - atan((X - L) / Y)) );
-
-    % Calculating I1
-    I1 = 1/(8 * pi) * ((X^2 + Y^2) * log(X^2 + Y^2) ...
-    - ((X - L)^2 + Y^2) * log((X - L)^2 + Y^2) ...
-    - 2 * X * L + L^2);
-
-    % Influence coefficient fa
-    infa = (I0*(1-(X/L)) - (I1/L));
-
-    % Influence coefficient fb
-    infb = (I0*(X/L) + (I1/L));
+    [infa,infb] = refpaninf(L,X,Y);
   
 end
 
-function lhsmat = build_lhs(xs, ys)
-    np = length(xs) - 1;             % Number of panels
-    psip = zeros(np, np+1);          % Intermediate matrix
-
-    % Loop over control points
-    for i = 1:np
-        xi = 0.5 * (xs(i) + xs(i+1));
-        yi = 0.5 * (ys(i) + ys(i+1));
-
-        % Loop over all panels
-        for j = 1:np
-            xj1 = xs(j);
-            yj1 = ys(j);
-            xj2 = xs(j+1);
-            yj2 = ys(j+1);
-
-            % Get influence coefficients
-            [infa, infb] = panelinf(xi, yi, xj1, yj1, xj2, yj2);
-
-            % Fill appropriate columns (note: influences from gamma_j and gamma_{j+1})
-            psip(i,j)   = psip(i,j)   + infa;  % influence from gamma_j
-            psip(i,j+1) = psip(i,j+1) + infb;  % influence from gamma_{j+1}
-        end
-    end
-
-    % Now add two additional rows for auxiliary conditions
-    lhsmat = zeros(np+1, np+1);
-
-    % Copy calculated part (np equations)
-    lhsmat(1:np,:) = psip;
-
-    % Final two rows: closure conditions
-    % Eq (7): gamma(np+1) = gamma(1)
-    lhsmat(np+1,1) = 1;
-    lhsmat(np+1,end) = -1;
-
-    % Eq (8): sum of all gamma = 0 (for circulation-free flow)
-    % Add if required, or impose different Kutta condition for airfoils
-end
-
-lhsmat = build_lhs(xs, ys);
-
-function rhsvec = build_rhs(xs, ys, alpha)
+function lhsmat = build_lhs(xs,ys)
+    %build_lhs Obtains the lhs of the streamfunction and gamma equation
+    %   
+    
     np = length(xs) - 1;
-    rhsvec = zeros(np+1, 1);  % Column vector
-
-    % Freestream streamfunction at control points (panel starting points)
-    for i = 1:np-1
-        xi = xs(i);
-        yi = ys(i);
-        psi_fs1 = yi * cos(alpha) - xi * sin(alpha);
-
-        xi1 = xs(i+1);
-        yi1 = ys(i+1);
-        psi_fs2 = yi1 * cos(alpha) - xi1 * sin(alpha);
-
-        rhsvec(i) = psi_fs1 - psi_fs2;
-    end
-
-    % Boundary conditions
-    rhsvec(np) = 0;      % psi(np+1) = psi(1)
-    rhsvec(np+1) = 0;    % gamma(1) = 0
+    psip = zeros(np,np+1); %Initialise matrix to reduce computation times
+    
+    for i=1:np
+        for j=1:np+1    
+                
+                if j == 1 
+                    [infa,infb] = panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xs(i),ys(i));
+                    psip(i,j) = infa;
+                elseif j == np+1 %Accounts for index out of bounds
+                    [infa1,infb1] = panelinf(xs(j-1),ys(j-1),xs(j),ys(j),xs(i),ys(i));
+                    psip(i,j) = infb1;
+                else
+                    [infa,infb] = panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xs(i),ys(i));
+                    [infa1,infb1] = panelinf(xs(j-1),ys(j-1),xs(j),ys(j),xs(i),ys(i));
+                    psip(i,j) = infa + infb1;
+                end
+        end
+    end %Looping over all the points around the cylinder measuring the contribution from each`point vortex onto one single point of the cylinder
+    
+    %Kutta Condition
+    
+    lhsmat = zeros(np+1,np+1);
+    lhsmat(1, [1:3,np-1:np]) = [2, -2, 1, -1, 2];
+    lhsmat(np+1,[2:3,np-1:np+1]) = [-2, 1, -1, 2, -2];
+    
+    
+    for i=2:np
+        for j=1:np+1
+            lhsmat(i,j) = psip(i,j) - psip(i-1,j);
+        end
+    end %Set our lhs matrix to the difference of the streamfunctions contribution due to the pannels of two consecutive points
 end
 
-rhsvec = build_rhs(xs,ys,alpha);
+function rhsvec = build_rhs(xs,ys,alpha) 
+    %build_rhs Obtains the rhs the Streamfunction and gamma equation
+    %   Detailed explanation goes here
+    
+    np = length(xs) - 1; %Number of points we are dividing the cylinder into
+    rhsvec = zeros(np+1,1); %Initialise the vector to reduce computation times
+    psi = zeros(np+1,1);
+    
+    for i = 1:np+1
+        psi(i) = ys(i)*cos(alpha) - xs(i)*sin(alpha); %Free stream contribution to the streamfunction
+    end
+    
+    for i = 2:np
+        rhsvec(i) = psi(i-1) - psi(i); %The rhs of our equation will be the difference between the free stream contributions between two consecutive points
+    end
+
+end
+
+lhsmat = build_lhs(xs,ys);
+rhsvec = build_rhs(xs,ys,alpha); %Unsure 0s in right place
 
 A = build_lhs(xs,ys);
-b = build_rhs(xs,ys,alpha);
-gam = A\b;
+b_0 = build_rhs(xs,ys,0);
+b_a = build_rhs(xs,ys,alpha);
+
+gam_0 = A\b_0;
+gam_a = A\b_a;
+
+circulation_0 = 0;
+circulation_a = 0;
+
+L1 = 2*pi/(np);
+x = theta(2);
+L = sqrt(2*(1-cos(x)));
+for i = 1:np+1
+    circulation_0 = circulation_0 + gam_0(i)*L;
+    circulation_a = circulation_a + gam_a(i)*L;
+end
+
+% Plot psi
+figure(1)
+plot(theta/pi,gam_0)
+xlabel('x'), ylabel('y')
+title('Influence Coefficient Psi')
+axis equal
+
+% Plot psi
+figure(2)
+plot(theta/pi,gam_a)
+xlabel('x'), ylabel('y')
+title('Influence Coefficient Psi')
+axis equal
+
+for i = 1:np
+    for j = 1:np
+        xm(i,j) = xmin + (i-1)*(xmax-xmin)/(np-1);
+        ym(i,j) = ymin + (j-1)*(ymax-ymin)/(np-1);
+    end
+end
+
+% Plot psi
+figure(3)
+contour(xm', ym', A', c1)
+xlabel('x'), ylabel('y')
+title('Influence Coefficient Psi')
+axis equal
